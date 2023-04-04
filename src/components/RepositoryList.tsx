a6317d750b45c7cb6b20e2dad92aa8510c21efe0
Thaysa <thaysarafaele@gmail.com>
2023-04-04 10:19:18 -0400

    alterando componentes para typescript

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -5,8 +5,14 @@ import '../styles/repositories.scss';
 //https://api.github.com/orgs/RocketSeat/repos
 //https://api.github.com/users/ThaysaRafaele
 
+interface Repository{
+    name: string;
+    description: string;
+    html_url: string;
+}
+
 export function RepositoryList(){
-    const [repositories, setRepositories] = useState([]);
+    const [repositories, setRepositories] = useState<Repository[]>([]);
 
     useEffect(() => {
         fetch('https://api.github.com/orgs/RocketSeat/repos')
